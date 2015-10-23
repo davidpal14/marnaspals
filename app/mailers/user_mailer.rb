7d0219689ccae20c0d3c8a4d1f49297872df7dea
David Pal <davidscott.pal@gmail.com>
2015-10-23 13:03:17 -0400

    fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
 	def welcome_email(subscribers)
 	    @subscribers = subscribers
 	    @url  = 'http://www.marnaspals.com/'
-	    attachments['free_tips.pdf'] = File.read('https://s3.amazonaws.com/marnaspals-dpal/thetips.pdf')
+	    
 	    mail(to: @subscribers.email, subject: 'Welcome To The Pal Post')
 
 	end
